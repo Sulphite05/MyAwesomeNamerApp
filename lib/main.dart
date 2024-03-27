80b2697c635ff3c98da587c24a198d9451690ecf
Aqiba Abdul Qadir <aqi.edu.2005@gmail.com>
2024-03-27 22:57:09 +0500

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,8 @@ class HomePage extends StatelessWidget {
 }
 
 
-enum MenuAction {logout}
+enum MenuAction {logout, misc}
+
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
 
@@ -83,13 +84,32 @@ class _NotesViewState extends State<NotesView> {
         title: const Text("My Notes"),
         actions: [
           PopupMenuButton( 
-            onSelected: (value){
+            onSelected: (value) async{  // value is a an enumeration value, whatever I select from the enums values
               devtools.log(value.toString());
+             
+              switch(value){
+                
+                case MenuAction.logout:
+                  final shouldLogout = await showLogoutDialog(context);
+                  devtools.log(shouldLogout.toString());
+                  if(shouldLogout){
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/', 
+                      (route) => false);
+                  }
+                  break;
+                case MenuAction.misc:
+                  break;
+              }
             }, 
             itemBuilder: (context){
               return [const PopupMenuItem<MenuAction>(
               value: MenuAction.logout,  // coder sees this
               child: Text("Log Out"), // user sees this
+              ), const PopupMenuItem<MenuAction>(
+              value: MenuAction.misc,  // coder sees this
+              child: Text("Misc"), // user sees this
               )]; // since popmenuentry requires a list return type
           })
           ],
@@ -97,6 +117,29 @@ class _NotesViewState extends State<NotesView> {
 
     );
   }
+}
+
+Future<bool> showLogoutDialog(BuildContext context){
+  return showDialog<bool>(
+    context: context, 
+    builder: (context){
+      return AlertDialog(
+        title: const Text('Sign Out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.of(context).pop(false);
+          }, 
+          child: const Text('Cancel')),
+          
+          TextButton(onPressed: (){
+            Navigator.of(context).pop(true);  // to return a value to the show dialogue
+          }, 
+          child: const Text('Sign Out')),
+        ],
+      );
+    }
+    ).then((value) => value ?? false); // show dialogue's value can be null, for instance, i press somewhere else on the screen instead of the pop up
 }
 
 // class HomePage extends StatelessWidget {
