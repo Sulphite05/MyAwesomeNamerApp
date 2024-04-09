bb81c70507fb0848fee192b865e15f99b046d9b4
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-04-09 22:16:51 +0500

    step-11

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,14 +1,29 @@
 import "package:firebase_auth/firebase_auth.dart" show User;
 import "package:flutter/material.dart";
 
-// void test() {
-//   User;
-// }
 
 @immutable
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);  // factory method
+  const AuthUser({required this.isEmailVerified});
+  factory AuthUser.fromFirebase(User user) => AuthUser(isEmailVerified: user.emailVerified);  // factory method and fromFirebase need a user as arg to return emailVerified
 }
+
+// @immutable
+// class AuthUser {
+//   final bool isEmailVerified;
+//   const AuthUser(this.isEmailVerified);
+//   factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);  // factory method and fromFirebase need a user as arg to return emailVerified
+
+// void testing() {
+//   AuthUser(true); // this instance does not make any sense... What's true, what does it mean?
+// }
+
+// to fix this dart gives us required names parameters like this:
+// void testing() {
+//   AuthUser(isEmailVerified: true); // but syntax error may arise for commented class, 
+//since named parameter require to be enclosed in curly braces with a required type
+// }
+
+// }
 
