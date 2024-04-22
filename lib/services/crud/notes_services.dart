3035eeb7f42c6ff63c3ec289a3d2da7f5600bc6d
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-04-22 23:15:33 +0500

    step-17

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -7,8 +7,19 @@ class DatabaseUser {
   final int id;
   final String email;
 
-  DatabaseUser({
+  const DatabaseUser({
     required this.id,
     required this.email,
   });
+
+  DatabaseUser.from_row(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn]
+            as String; // when we read our database, we read hash tables for each row of the user
+
+  @override
+  String toString() => 'Person, id = $id, email = $email';
 }
+
+const idColumn = 'id';
+const emailColumn = 'email';
