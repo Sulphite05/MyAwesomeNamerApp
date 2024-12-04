4230a01d2dbeddeff9574b572acc8b7a0addcca4
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-12-04 20:23:40 +0500

    Renewed App

--- lib/constants/routes.dart
+++
@@ -1,5 +0,0 @@
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const notesRoute = '/notes/';
-const verifyEmailRoute = '/verify-email/';
-const newNotesRoute = '/notes/new-note/';
